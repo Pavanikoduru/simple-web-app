06b69e6493afd700aa0b692f7833064fbdb0d47f
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-10-18 15:35:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,26 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-###############################
-# Create IAM Role for EKS Node
-###############################
+###################################
+# Create VPC
+###################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.0"
+
+  name = "hello-world-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+}
+
+###################################
+# IAM Role for EKS Worker Nodes
+###################################
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
 
@@ -39,4 +56,58 @@ resource "aws_iam_role_policy_attachment" "eks_worker_attach" {
 
 resource "aws_iam_role_policy_attachment" "eks_cni_attach" {
   role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:_
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_registry_attach" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+###################################
+# Create EKS Cluster
+###################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.1.0"
+
+  cluster_name    = "hello-world-cluster"
+  cluster_version = "1.27"
+
+  vpc_id     = module.vpc.vpc_id
+  subnets    = module.vpc.private_subnets
+
+  node_groups = {
+    hello_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+      iam_role_arn     = aws_iam_role.eks_node_role.arn
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "hello-world"
+  }
+}
+
+###################################
+# Outputs
+###################################
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "kubeconfig_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
