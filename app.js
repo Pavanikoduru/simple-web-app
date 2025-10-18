8e9a25be446c17ff30acc9cfed7703e88a403a32
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-10-18 14:35:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
