39f62ee7b1d2c5e5954bb72d40fb3d9478da48bd
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-10-18 15:32:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,74 +1,42 @@
-<<<<<<< HEAD
-provider "aws" {
-  region = "ap-south-1"
-=======
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.3.0"
->>>>>>> 64ce9e7114bd9ee3cf831a5525dc3c25a48025b6
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
 
-# Create a simple VPC for EKS
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+###############################
+# Create IAM Role for EKS Node
+###############################
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-# Create EKS cluster
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
-
-  cluster_name    = "hello-world-cluster"
-  cluster_version = "1.27"
-
-  cluster_endpoint_public_access = true
-
-  vpc_id  = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-
-  eks_managed_node_groups = {
-    default = {
-<<<<<<< HEAD
-      desired_capacity = 1
-      max_capacity     = 2
-      min_capacity     = 1
-      instance_type    = "t2.micro"
-=======
-      desired_size = 1
-      max_size     = 2
-      min_size     = 1
-
-      instance_types = ["t3.micro"]
->>>>>>> 64ce9e7114bd9ee3cf831a5525dc3c25a48025b6
-    }
-  }
+resource "aws_iam_role_policy_attachment" "eks_worker_attach" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "kubeconfig" {
-  value     = module.eks.kubeconfig
-  sensitive = true
-}
+resource "aws_iam_role_policy_attachment" "eks_cni_attach" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:_
